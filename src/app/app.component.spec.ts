3f54549d7f39b425a5ebaa5dfebb672796f7bb2d
wala-amiri <walaamiri02@gmail.com>
2023-12-15 13:29:09 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Workshop2'`, () => {
+  it(`should have as title 'Workshop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Workshop2');
+    expect(app.title).toEqual('Workshop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Workshop2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Workshop app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ResidencesComponentComponent } from './residences-component/residences-component.component';
+import { FormsModule } from '@angular/forms';
+import { FormResidenceComponent } from './form-residence/form-residence.component';
+import { NavbarComponent } from './navbar/navbar.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResidencesComponentComponent,
+    FormResidenceComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
